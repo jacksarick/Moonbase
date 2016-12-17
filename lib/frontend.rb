2737843516170241616bef326b2c13368223e1bd
Jack Sarick <jacksarick@gmail.com>
2016-12-16 23:52:23 -0500

    No one needs to see that

--- lib/frontend.rb
+++ lib/frontend.rb
@@ -7,14 +7,10 @@ class Frontend
 
 	def response(request)
 
-		# Special index case
+		# Special index cases
 		if ["/", "/dashboard.html"].include? request[1]
 			request[1] = '/index.html'
 		end
-
-		# print "###########################"
-		# print request[1]
-		# print "###########################"
 
 		database = read_YAML(read_YAML("config.yml")["database"])
 		@socket.print http_compose request[1], database
