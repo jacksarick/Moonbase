d24b915710c3a5dbe241f8dfea07a771a8716f52
Jack Sarick <jacksarick@gmail.com>
2016-12-16 23:16:11 -0500

    Database now load automagically

--- lib/frontend.rb
+++ lib/frontend.rb
@@ -3,15 +3,14 @@ require_relative './composer.rb'
 class Frontend
 	def initialize(socket)
 		@socket = socket
-		@config = read_YAML("config.yml")
-
 	end
 
 	def response(request)
 		# Special index case
 		case request[1].split('').last
 		when '/'
-			@socket.print http_compose request[1] + 'index.html', read_YAML("./user/projects.yml")
+			database = read_YAML(read_YAML("config.yml")["database"])
+			@socket.print http_compose request[1] + 'index.html', database
 		else
 			@socket.print http_compose request[1]
 		end
