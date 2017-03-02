34621165473ba2248569e8563e046f724519a43e
Jack Sarick <jacksarick@gmail.com>
2017-03-01 22:21:40 -0500

    It works

--- lib/https-server.rb
+++ lib/https-server.rb
@@ -2,13 +2,14 @@ require 'socket'
 require 'openssl'
 require_relative './utility.rb'
 
-# Load config
-config = read_YAML("config.yml")
 
 class Server
 	def initialize(address, port)
 		@address = address
 		@port = port
+
+		# Load config
+		@config = read_YAML("config.yml")
 	end
 
 	def start
@@ -16,11 +17,12 @@ class Server
 		tcps = TCPServer.new(@address, @port)
 
 		# Load up certs
-		ssl_context = OpenSSL::SSL::SSLContext.new()
-		ssl_context.cert = OpenSSL::X509::Certificate.new(File.open(config['cert']))
-		ssl_context.key = OpenSSL::PKey::RSA.new(File.open(config['key']))
+		ssl_context = OpenSSL::SSL::SSLContext.new
+		ssl_context.cert = OpenSSL::X509::Certificate.new(File.open(@config['cert']))
+		ssl_context.key = OpenSSL::PKey::RSA.new(File.open(@config['key']))
 		ssl_context.ssl_version = :SSLv23
 
+		# Start server
 		ssls = OpenSSL::SSL::SSLServer.new(tcps, ssl_context)
 		ssls.start_immediately = true
 
