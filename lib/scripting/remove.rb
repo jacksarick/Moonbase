4972e6b0625d496f7b00e47f6d20b28c01d4b08a
Jack Sarick <jacksarick@gmail.com>
2017-03-10 09:47:52 -0500

    Commented remove script

--- lib/scripting/remove.rb
+++ lib/scripting/remove.rb
@@ -4,12 +4,15 @@ require "yaml"
 require_relative '../utility.rb'
 @config = read_YAML("config.yml")
 
+# Grab arguments
 project = ARGV[0]
 projects = read_YAML(@config["database"])
 
+# Remove info from database file
 write_YAML @config["database"], projects.select { |e| e["project"] != project }
 
+# Go to the project folder
 Dir.chdir("#{@config['projects']}")
 
+# Remove it with force
 puts `rm -rf #{project}`
-
